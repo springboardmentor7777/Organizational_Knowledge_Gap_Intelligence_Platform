8483c879b06101429211a982fc97e34c220424f6
Siridasari54 <siri.dasari43@gmail.com>
2026-07-07 22:47:57 +0530

    Added responsive Navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,21 @@
+function Navbar(){
+
+return(
+
+<nav className="navbar navbar-dark bg-primary">
+
+<div className="container-fluid">
+
+    <span className="navbar-brand text-wrap text-center text-sm-start w-100 w-sm-auto">
+      Organizational Knowledge Gap Intelligence Platform
+    </span>
+
+</div>
+
+</nav>
+
+)
+
+}
+
+export default Navbar;
